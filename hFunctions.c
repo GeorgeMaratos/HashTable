cf34dabdf3fa7579c57ed8ca3cbea06d50260190
George Maratos <gmaratos@comcast.net>
2015-01-15 07:46:50 -0600

    Added improved insert and debug function

--- hFunctions.c
+++ hFunctions.c
@@ -31,4 +31,35 @@ hashFunction(int val,int (*f)(int))
 }
 
 //hash table ops
+void
+insertValue(int index, int value, Head *hd)
+{
+  //variables
+  Node *nd;
+  //ops
+  if(index == hd->capacity) return;
+  if(hd->list[index] != NULL) insertValue(index + 1, value, hd);
+  else
+  {
+    nd = malloc(sizeof(Node));
+    nd->val  = value;
+    nd->next = NULL;
+    hd->list[index] = nd;
+  }
+}
 
+//debugging function
+void
+printTable(Head *hd)
+{
+  //variables
+  int i;
+  //ops
+  if(hd == NULL) return;
+  if(hd->list == NULL) return;
+  for(i=0;i<capacity;i++)
+  {
+    if(hd->list[i] != NULL)
+      printf("%d : %d\n", i, hd->list[i]->val);
+  }
+}
